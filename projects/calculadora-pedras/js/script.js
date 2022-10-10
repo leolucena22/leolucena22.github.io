f2281b676cf85b9045eabb2a1cc85e6f463ef4fb
leolucena22 <leonardolucena44@gmail.com>
2022-10-10 19:12:51 -0300

    Alterado tipo de calculo

--- projects/calculadora-pedras/js/script.js
+++ projects/calculadora-pedras/js/script.js
@@ -1,40 +1,39 @@
-let pedras = document.querySelector('#qtd-pedras')
+let metros = document.querySelector('#qtd-metros')
 
 let resultado = document.querySelector('#result')
 
 function imprimeResultado(valor) {
-  resultado.innerHTML = `${valor} Metros`
+  resultado.innerHTML = `Quantidade🪨= ${valor} `
 }
 
 function limpar() {
-  pedras.value = ''
+  metros.value = ''
   resultado.innerHTML = ''
 }
 
 function vinteQuarenta() {
-  let calculo = Number(pedras.value) / 12.5
+  let calculo = Number(metros.value) * 12.5
   calculo <= 0
     ? (resultado.innerHTML = `Valor Inválido`)
     : imprimeResultado(calculo.toFixed(2))
 }
 
 function trinta() {
-  let calculo = Number(pedras.value) / 11.11
+  let calculo = Number(metros.value) * 11.11
   calculo <= 0
     ? (resultado.innerHTML = `Valor Inválido`)
     : imprimeResultado(calculo.toFixed(2))
 }
 
 function quarenta() {
-  let calculo = Number(pedras.value) / 6.25
+  let calculo = Number(metros.value) * 6.25
   calculo <= 0
     ? (resultado.innerHTML = `Valor Inválido`)
     : imprimeResultado(calculo.toFixed(2))
 }
 
 function cinquenta() {
-  let calculo = Number(pedras.value) / 4.0
-  let metros_total = Number(pedras * 4.0)
+  let calculo = Number(metros.value) * 4.0
   calculo <= 0
     ? (resultado.innerHTML = `Valor Inválido`)
     : imprimeResultado(calculo.toFixed(2))
