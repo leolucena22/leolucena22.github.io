e7deebd523bd6bbbddf15983d1eed9559e8309dc
leolucena22 <leonardolucena44@gmail.com>
2022-10-10 00:02:48 -0300

    Add project calculadora de pedras

---
+++ projects/calculadora-pedras/js/script.js
@@ -0,0 +1,35 @@
+let pedras = document.querySelector('#qtd-pedras')
+
+let resultado = document.querySelector('#result')
+
+function imprimeResultado(valor) {
+  resultado.innerHTML = `${valor} Metros`
+}
+
+function vinteQuarenta() {
+  let calculo = Number(pedras.value) / 12.5
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function trinta() {
+  let calculo = Number(pedras.value) / 11.11
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function quarenta() {
+  let calculo = Number(pedras.value) / 6.25
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function cinquenta() {
+  let calculo = Number(pedras.value) / 4.0
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
