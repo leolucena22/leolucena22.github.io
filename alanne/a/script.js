3d67c863719229109354e08661352be3c07e5e41
leolucena22 <leonardolucena44@gmail.com>
2024-05-31 20:07:16 -0300

    remove pages

--- alanne/a/script.js
+++
@@ -1,9 +0,0 @@
-let button = document.getElementById('nao')
-let height = window.innerHeight - 50
-let width = window.innerWidth - 50
-
-button.addEventListener('mouseover', function () {
-  button.style.position = 'absolute'
-  button.style.top = Math.random() * height + 'px'
-  button.style.left = Math.random() * width + 'px'
-})
